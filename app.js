913c731af033cf6a321a2b9333ff42b100ada106
Ankit-Nag <ankitknag@gmail.com>
2020-12-18 00:03:22 +0530

    completed app
    feat: added funtranslation API

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 //define our variables
 var btnTranslate = document.querySelector("#btn-translate");
-var inputText= document.querySelector("#text-input");
-var outputText= document.querySelector("#text-output");
+var inputText = document.querySelector("#text-input");
+var outputText = document.querySelector("#text-output");
+var serverURL = "https://api.funtranslations.com/translate/pirate.json";
+
+//create server URL for querying [load]
+function getTranslationURL(text) {
+    return serverURL + "?text=" + text;
+}
+
+//error handling
+function errorHandler(error){
+    console.log("Error: ",error);
+}
 
 //click event
 function clickEventHandler() {
-    console.log(inputText.value);
-    outputText.innerText=inputText.value;
+    fetch(getTranslationURL(inputText.value))
+    .then(response => response.json())
+    .then(json => console.log(json))
+    .catch(errorHandler)
 }
 
 btnTranslate.addEventListener("click", clickEventHandler);
